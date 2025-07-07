771b8aa8dfbeec6c3d5ac749d355f9880ecbee32
veiveid <vc.cn@163.com>
2025-07-07 14:42:44 +0800

    Create UserMapper.java
    UserMapper

---
+++ src/main/java/com/atguigu/springboot/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.atguigu.springboot.mapper;
+
+import com.atguigu.springboot.dao.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author : zhangwei
+ * @date : 2025-07-04
+ **/
+public interface UserMapper extends BaseMapper<User> {
+}
